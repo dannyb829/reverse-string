babdf173964ecbb1e82cc8f15cca6b8c2f58df73
Daniel <dannyb829@gmail.com>
2022-06-01 16:36:34 -0400

    commit tree 5c63b7a9165ddd99fa7ce2f923dc9939437a20f9

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,12 @@
 function reverseString(str) {
   // type your code here
+  reversedString = ""
+
+    for (let i = str.length - 1; i > -1; --i) {
+      reversedString + str[i]
+    }
+    
+  return reversedString
 }
 
 if (require.main === module) {
@@ -17,3 +24,7 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+// the goal is have a given string backwards
+// if i can grab each character starting from the end
+//  each time adding it to an initially empty string then i can 
+// achieve the reveresed string
